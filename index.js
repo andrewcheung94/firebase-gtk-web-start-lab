c9d7fd7967c069bcc390670dbd14c81931ba909e
andrewcheung94 <cheung.andrew94@gmail.com>
2021-06-13 22:57:56 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,103 @@
+// Import stylesheets
+import "./style.css";
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+import * as firebaseui from "firebaseui";
+
+// Document elements
+const startRsvpButton = document.getElementById("startRsvp");
+const guestbookContainer = document.getElementById("guestbook-container");
+
+const form = document.getElementById("leave-message");
+const input = document.getElementById("message");
+const guestbook = document.getElementById("guestbook");
+const numberAttending = document.getElementById("number-attending");
+const rsvpYes = document.getElementById("rsvp-yes");
+const rsvpNo = document.getElementById("rsvp-no");
+
+var rsvpListener = null;
+var guestbookListener = null;
+
+async function main() {
+  // Add Firebase project configuration object here
+  var firebaseConfig = {
+    apiKey: "AIzaSyDAC6hTAI5o2JQILZzxW_pR8SQPg-iaGj4",
+    authDomain: "shopping-list-9d215.firebaseapp.com",
+    projectId: "shopping-list-9d215",
+    storageBucket: "shopping-list-9d215.appspot.com",
+    messagingSenderId: "94888397354",
+    appId: "1:94888397354:web:71ae1f585b000fe72bd153",
+    measurementId: "G-TX1C9H0H81"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // FirebaseUI config
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      // Email / Password Provider.
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // Handle sign-in.
+        // Return false to avoid redirect.
+        return false;
+      }
+    }
+  };
+  // Initialize the FirebaseUI widget using Firebase
+  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+  // Called when the user clicks RSVP button 
+  startRsvpButton.addEventListener("click",
+  () => {
+      if (firebase.auth().currentUser) {
+      // User is signed in; allows user to sign out
+      firebase.auth().signOut();
+    } else {
+      // No user is signed in; allows user to sign in
+      ui.start("#firebaseui-auth-container", uiConfig);
+    }
+  });
+
+  // Listen to the current Auth state
+  firebase.auth().onAuthStateChanged((user)=> {
+    if (user) {
+      startRsvpButton.textContent = "LOGOUT";
+      // Show guestbook to logged-in users
+      guestbookContainer.style.display = "block";
+    }
+    else {
+      startRsvpButton.textContent = "RSVP";
+      // Hide guestbook from non-logged-in users
+      guestbookContainer.style.display = "none";
+    }
+  });
+
+  // Listen to the form submission
+  form.addEventListener("submit", (e) => {
+  // Prevent the default form redirect
+  e.preventDefault();
+  // Write a new message to the database collection "guestbook"
+  firebase.firestore().collection("guestbook").add({
+    text: input.value,
+    timestamp: Date.now(),
+    name: firebase.auth().currentUser.displayName,
+    userId: firebase.auth().currentUser.uid
+  })  
+  // clear message input field
+  input.value = "";
+  // Return false to avoid redirect
+  return false;
+  });
+
+}
+main();
